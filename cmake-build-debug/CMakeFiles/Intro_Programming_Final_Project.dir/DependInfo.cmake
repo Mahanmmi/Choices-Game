6e937a95cab33a89bb7197ad9ec7123dab1b8697
Mahanmmi <mahanmmi@users.noreply.github.com>
2019-01-02 22:26:54 +0330

    Added init game / save and load game

---
+++ cmake-build-debug/CMakeFiles/Intro_Programming_Final_Project.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "K:/MAHAN/Uni/Term 1/Introduction to Programming/Final Project/Intro_Programming_Final_Project/MyFile.c" "K:/MAHAN/Uni/Term 1/Introduction to Programming/Final Project/Intro_Programming_Final_Project/cmake-build-debug/CMakeFiles/Intro_Programming_Final_Project.dir/MyFile.c.obj"
+  "K:/MAHAN/Uni/Term 1/Introduction to Programming/Final Project/Intro_Programming_Final_Project/MyLinkedList.c" "K:/MAHAN/Uni/Term 1/Introduction to Programming/Final Project/Intro_Programming_Final_Project/cmake-build-debug/CMakeFiles/Intro_Programming_Final_Project.dir/MyLinkedList.c.obj"
+  "K:/MAHAN/Uni/Term 1/Introduction to Programming/Final Project/Intro_Programming_Final_Project/main.c" "K:/MAHAN/Uni/Term 1/Introduction to Programming/Final Project/Intro_Programming_Final_Project/cmake-build-debug/CMakeFiles/Intro_Programming_Final_Project.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Intro_Programming_Final_Project.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Intro_Programming_Final_Project.dir/main.c.obj"
+  "CMakeFiles/Intro_Programming_Final_Project.dir/MyFile.c.obj"
+  "CMakeFiles/Intro_Programming_Final_Project.dir/MyLinkedList.c.obj"
+  "Intro_Programming_Final_Project.pdb"
+  "Intro_Programming_Final_Project.exe"
+  "Intro_Programming_Final_Project.exe.manifest"
+  "libIntro_Programming_Final_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Intro_Programming_Final_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
